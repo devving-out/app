47bf96b8ccbd56116c351a1f3bac90d97e65dfad
Taylor Birkeland <tabirkeland@gmail.com>
2015-04-07 02:09:52 +0000

    fix upload lead

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/taylor/git/app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/taylor/git/app/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/taylor/git/app/platforms/android/ant-build/AndroidManifest.xml : \
+/home/taylor/git/app/platforms/android/AndroidManifest.xml \
